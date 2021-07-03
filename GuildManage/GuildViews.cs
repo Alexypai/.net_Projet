186e6960f4954f1585bb6f1f4c4977db6389465c
Alexypai <72445670+Alexypai@users.noreply.github.com>
2021-07-03 16:57:42 +0200

    Add files via upload

---
+++ GuildManage/GuildViews.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuildManage
+{
+    public partial class GuildeView : Form
+    {
+        public GuildeView()
+        {
+            InitializeComponent();
+        }
+
+        private void GuildeView_Load(object sender, EventArgs e)
+        {
+            GuildeManageEntities entities = new GuildeManageEntities();
+            List<Héros> listHéros = entities.Héros.ToList();
+            dataHeroGrid.DataSource = listHéros;
+        }
+    }
+}
